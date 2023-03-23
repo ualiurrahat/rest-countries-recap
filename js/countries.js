cc6934e87464f3a450769c70c9ce47f628e3b69e
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-23 21:10:49 +0600

    explored destructuring and sent data to html elements using props

--- js/countries.js
+++ js/countries.js
@@ -12,13 +12,40 @@ const displayCountries = countries => {
     countryContainer.innerHTML = countriesHTML.join(' ');
 
 }
+// option1: accessing countries common name and flags 
+// const getCountryHTML = country => {
+//     return `
+//         <div class = "country">
+//             <h2>${country.name.common}</h2>
+//             <img src = "${country.flags.png}">
+//         </div>
+//     `
+// }
 
-const getCountryHTML = country => {
+// option 2: accesing countries common name and flags using destructuring
+// const getCountryHTML = country => {
+//     const { name, flags } = country;
+//     return `
+//         <div class = "country">
+//             <h2>${name.common}</h2>
+//             <img src = "${flags.png}">
+//         </div>
+//     `
+
+// }
+// option 3: accesing countries common name and flags using destructuring directly as parameter
+const getCountryHTML = ({ name, flags,area,region }) => {
+
     return `
         <div class = "country">
-            <h2>${country.name.common}</h2>
-            <img src = "${country.flags.png}">
+            <h2>${name.common}</h2>
+            <p>${area}</p>
+            <p>Contitnent: ${region}</p>
+            <img src = "${flags.png}">
         </div>
     `
+
 }
+
+
 loadCountries();
