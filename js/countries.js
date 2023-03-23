38a6a36765dc724f537cf3779d0b577e908a4c9a
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-23 19:20:03 +0600

    dynamically created html elements based on data

---
+++ js/countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    // console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML[0]);
+    const countryContainer = document.getElementById('countries');
+    countryContainer.innerHTML = countriesHTML.join(' ');
+
+}
+
+const getCountryHTML = country => {
+    return `
+        <div class = "country">
+            <h2>${country.name.common}</h2>
+            <img src = "${country.flags.png}">
+        </div>
+    `
+}
+loadCountries();
